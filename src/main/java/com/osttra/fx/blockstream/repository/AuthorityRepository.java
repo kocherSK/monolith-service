9504dae825d8d187e6f436e273b5f5be7c43967d
Subhash Kocher <subhash.kocher@ihsmarkit.com>
2023-02-08 21:25:04 +0530

    Initial version of service generated by generator-jhipster@7.9.3

---
+++ src/main/java/com/osttra/fx/blockstream/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.osttra.fx.blockstream.repository;
+
+import com.osttra.fx.blockstream.domain.Authority;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+/**
+ * Spring Data MongoDB repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends MongoRepository<Authority, String> {}
---
+++ src/test/java/com/osttra/fx/blockstream/IntegrationTest.java
@@ -0,0 +1,22 @@
+package com.osttra.fx.blockstream;
+
+import com.osttra.fx.blockstream.ServiceApp;
+import com.osttra.fx.blockstream.config.AsyncSyncConfiguration;
+import com.osttra.fx.blockstream.config.EmbeddedMongo;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { ServiceApp.class, AsyncSyncConfiguration.class })
+@EmbeddedMongo
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
